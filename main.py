559bab350a190a5dd58d01ecc498a2333e6b55fb
TelvinMaina <83200575+Telvin088@users.noreply.github.com>
2025-07-01 09:25:49 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, session, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blog_project'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
@@ -8,4 +17,6 @@ def index():
     return 'Hello from Flask!'
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=5000)
